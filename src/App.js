2bd3da771bc99acb631aa9ff39110f67c51435e0
andy_hardy <aohardy@gmail.com>
2019-07-22 18:15:17 -0600

    setup files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav/Nav';
+import Main from './components/Main/Main';
+
+function App() {
+    return (
+        <>
+            <Nav />
+            <Main />
+        </>
+    );
+}
+
+export default App;
