32b26dda7fd66a3ca36832965c0d948e19bc9236
andy_hardy <aohardy@gmail.com>
2019-07-24 07:42:07 -0600

    working on getting the entry background to look right

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -12,7 +12,7 @@ class Entry extends React.Component {
                         <h5 className="card-title">Sharp your memory is??</h5>
                         <p className="card-text">To Play: Click on any character on the screen.  The characters will re-jumble and
                         show up in new places.  Every time you click on a new character, your score goes up by one.  Keep on clicking until you click all twelve characters to win!</p>
-                        <p className="card-text">Be careful though... If you click a character twice, you lose! Your high score is shown along with the current game score, in the upper
+                        <p className="card-text">Be careful though... if you click a character twice, you lose! Your high score is shown along with the current game score, in the upper
                         right corner of the screen.</p>
                         <p className="card-text">Happy clicking!!!</p>
                         <Link to="/game" className="btn btn-primary">Play the Game!</Link>
